b55618d22a1262471dfc933717f629aa693dd5c2
Garab98 <antrodaribau@gmail.com>
2024-08-31 03:28:00 +0200

    deployment

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ app = Flask(__name__)
 app.register_blueprint(user_blueprint, url_prefix='/users')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=5000)
